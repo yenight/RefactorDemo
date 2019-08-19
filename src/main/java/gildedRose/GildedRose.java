5887f81fd58c999247cea5e0aec31c50b42b9b3a
williamye <william.ye@oocl.com>
2019-08-19 19:07:39 +0800

    [refactor: if condition extract to method(quantity < 0,quantity > 50, !item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE))]

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -12,26 +12,25 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACKSTAGE)) {
-                if (items[i].quality > 0) {
+            if (isNotAgedBrieOrBackstage(items[i])) {
+                if (isQuantityMoreThanZero(items[i])) {
                     if (!items[i].name.equals(SULFURAS)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (isQuantityLessThanFifty(items[i])) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals(BACKSTAGE)) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (isQuantityLessThanFifty(items[i])) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                            if (isQuantityLessThanFifty(items[i])) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -46,7 +45,7 @@ public class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals(AGED_BRIE)) {
                     if (!items[i].name.equals(BACKSTAGE)) {
-                        if (items[i].quality > 0) {
+                        if (isQuantityMoreThanZero(items[i])) {
                             if (!items[i].name.equals(SULFURAS)) {
                                 items[i].quality = items[i].quality - 1;
                             }
@@ -55,11 +54,24 @@ public class GildedRose {
                         items[i].quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (isQuantityLessThanFifty(items[i])) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
+    }
+
+    private boolean isQuantityLessThanFifty(Item item) {
+        return item.quality < 50;
+    }
+
+    private boolean isQuantityMoreThanZero(Item item) {
+        return item.quality > 0;
+    }
+
+    private boolean isNotAgedBrieOrBackstage(Item item) {
+        return !item.name.equals(AGED_BRIE)
+                && !item.name.equals(BACKSTAGE);
     }
 }
