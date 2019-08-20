76244ab257b869b28f14f445993fec07cf3ac18c
williamye <william.ye@oocl.com>
2019-08-20 08:40:19 +0800

    [refactor: backstage strategy class]

--- src/main/java/gildedRose/BackstageStrategy.java
+++ src/main/java/gildedRose/BackstageStrategy.java
@@ -5,12 +5,12 @@ public class BackstageStrategy extends GildedRoseStrategy {
     public void updateQuality(Item item) {
         if (item.isQuantityLessThanFifty()) {
             item.addQuantity();
-            if (item.isSellInLessThanNumber(11) && item.isQuantityLessThanFifty()) {
-                item.addQuantity();
-            }
-            if (item.isSellInLessThanNumber(6) && item.isQuantityLessThanFifty()) {
-                item.addQuantity();
-            }
+        }
+        if (item.isSellInLessThanNumber(11) && item.isQuantityLessThanFifty()) {
+            item.addQuantity();
+        }
+        if (item.isSellInLessThanNumber(6) && item.isQuantityLessThanFifty()) {
+            item.addQuantity();
         }
         item.subtractSellIn();
         if (item.isSellInLessThanNumber(0)) {
